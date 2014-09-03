e6c7461f0b2caef2ab9169f07d16b2483dac3a89
Egor Pasko <pasko@chromium.org>
2014-09-03 20:05:51 +0200

    Generate and use profile
    Assumes the symbol __gcov_dump_all exists.

--- library.c
+++ library.c
@@ -13,6 +13,8 @@ int64_t TimeDiffMicros(const struct timespec* start, const struct timespec* end)
   return 1000000 * sec_diff + (end->tv_nsec - start->tv_nsec) / 1000;
 }
 
+void __attribute__((weak)) __gcov_dump_all();
+
 void __attribute__ ((visibility ("default"))) RunBenchmark() {
   const size_t kNumIterations = 1024 * 1024 * 256;
   struct timespec start, end;
@@ -29,4 +31,11 @@ void __attribute__ ((visibility ("default"))) RunBenchmark() {
   int64_t time_millis = TimeDiffMicros(&start, &end) / 1000;
 
   printf("PASSED (%" PRId64 "ms)\n", time_millis);
+  if (__gcov_dump_all) {
+    printf("Writing profile to disk...\n");
+    __gcov_dump_all();
+    printf("Profile written.\n");
+  } else {
+    printf("No profile information to dump.\n");
+  }
 }
